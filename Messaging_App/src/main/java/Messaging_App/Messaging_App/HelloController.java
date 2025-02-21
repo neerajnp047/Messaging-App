123654d081c54fc314861e3478a76a7bb124f33d
neerajnp047 <neerajnp047@gmail.com>
2025-02-21 16:10:41 +0530

    [Neeraj Prajapati] GET Request Method and pass name as query parameter

--- Messaging_App/src/main/java/Messaging_App/Messaging_App/HelloController.java
+++ Messaging_App/src/main/java/Messaging_App/Messaging_App/HelloController.java
@@ -1,12 +1,23 @@
 package Messaging_App.Messaging_App;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class    HelloController {
+    //UC-1 Use GET Request Method
     @RequestMapping("/hello")
     public String hello (){
         return "Hello From Bridgelabz";
     }
+
+    //UC-2 Use GET Request Method and pass name as query parameter
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(defaultValue = "Neeraj") String name){
+        return "Hello " + name;
+
+    }
+
 }
