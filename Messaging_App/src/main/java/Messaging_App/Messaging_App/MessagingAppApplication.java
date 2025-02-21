56028cc9095d3348245b0934349db547bd1122d7
neerajnp047 <neerajnp047@gmail.com>
2025-02-21 15:43:36 +0530

    [Neeraj Prajapati] UC-01 Get Hello Request API

---
+++ Messaging_App/src/main/java/Messaging_App/Messaging_App/MessagingAppApplication.java
@@ -0,0 +1,13 @@
+package Messaging_App.Messaging_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAppApplication.class, args);
+	}
+
+}
---
+++ Messaging_App/src/test/java/Messaging_App/Messaging_App/MessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Messaging_App.Messaging_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
